50822bbbeb5218879b3021d0c1a0da4b728ef16f
juanova0306 <131073151+juanova0306@users.noreply.github.com>
2025-02-07 20:12:01 -0600

    Update settings.py

--- django_crud/settings.py
+++ django_crud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z*de7!=m=im5l*zppg3r($kh^kbi8=#e6p^l8eo$y2014v@t@_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 PORT = os.getenv("PORT", "8000")
 
 # Application definition
